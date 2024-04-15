fb4b7df4a6f033118b9f5f4c21c31a58610f00bf
Aristofany Herderson <programmeraristofanyherderson@gmail.com>
2024-04-15 10:24:08 -0300

    :hammer: refactor: update filters implementation and performance

--- src/components/misc/pokemons-load/index.tsx
+++ src/components/misc/pokemons-load/index.tsx
@@ -11,7 +11,7 @@ import {
   SELECTPOKEMONHEIGHTS,
   SELECTPOKEMONWEIGHTS,
 } from "@/utils/selects-data";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { PokemonCard } from "../pokemon-card";
 import styles from "./styles.module.scss";
@@ -26,12 +26,138 @@ export const PokemonsLoad = () => {
   const { search, from, to, type, weakness, ability, weight, height, order } =
     usePokemonQueryParams();
 
+  const filteredPokemons = useMemo(() => {
+    return pokemons
+      .filter((pokemon) => {
+        if (!search?.trim() || search == null) {
+          return true;
+        } else if (pokemon.name.toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const fromVerification =
+          from != null ? pokemon.id >= from : pokemon.id >= 0;
+        const toVerification =
+          to != null ? pokemon.id <= to : pokemon.id <= MAXPOKEMONSRENDERED;
+
+        if (fromVerification && toVerification) {
+          return true;
+        }
+
+        if (from == null && to == null) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const types = pokemon.types.map((type) => type.type.name);
+        const currentType = type?.split(",") as PokemonPossibleTypes[];
+        const typeExists = currentType?.every((type) => types?.includes(type));
+
+        if (currentType?.length > 0 && typeExists) {
+          return true;
+        }
+
+        if (currentType == null) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const weaknesses = pokemon.weakness;
+        const currentWeakness = weakness?.split(",") as PokemonPossibleTypes[];
+        const weaknessExists = currentWeakness?.every((type) =>
+          weaknesses?.includes(type)
+        );
+
+        if (currentWeakness?.length > 0 && weaknessExists) {
+          return true;
+        }
+
+        if (currentWeakness == null) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const abilityList = pokemon.abilities.map((ability) =>
+          ability.ability.name.toLowerCase()
+        );
+        const currentAbility = ability as string;
+
+        if (
+          ability != null &&
+          abilityList.includes(currentAbility.toLowerCase())
+        ) {
+          return true;
+        }
+
+        if (ability == null || ability == undefined) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const currentWeight = weight;
+        const pokemonHeight = pokemon.weight / 10;
+
+        const minVerification =
+          currentWeight != null &&
+          pokemonHeight > SELECTPOKEMONWEIGHTS[currentWeight - 1]?.range?.min;
+        const maxVerification =
+          currentWeight != null &&
+          pokemonHeight <= SELECTPOKEMONWEIGHTS[currentWeight - 1]?.range?.max;
+
+        if (minVerification && maxVerification) {
+          return true;
+        }
+
+        if (currentWeight == null) {
+          return true;
+        }
+
+        return false;
+      })
+      .filter((pokemon) => {
+        const currentHeight = height;
+        const pokemonHeight = pokemon.height / 10;
+
+        const minVerification =
+          currentHeight != null &&
+          pokemonHeight > SELECTPOKEMONHEIGHTS[currentHeight - 1]?.range?.min;
+        const maxVerification =
+          currentHeight != null &&
+          pokemonHeight <= SELECTPOKEMONHEIGHTS[currentHeight - 1]?.range?.max;
+
+        if (minVerification && maxVerification) {
+          return true;
+        }
+
+        if (currentHeight == null) {
+          return true;
+        }
+
+        return false;
+      });
+  }, [pokemons, ability, from, height, search, to, type, weakness, weight]);
+
   useEffect(() => {
     const DESCPokemons = () => {
-      setPokemons((prevState) => prevState?.sort((a, b) => b.id - a.id));
+      setPokemons((prevPokemons) =>
+        prevPokemons?.slice().sort((a, b) => b.id - a.id)
+      );
     };
     const ASCPokemons = () => {
-      setPokemons((prevState) => prevState?.sort((a, b) => a.id - b.id));
+      setPokemons((prevPokemons) =>
+        prevPokemons?.slice().sort((a, b) => a.id - b.id)
+      );
     };
 
     order?.toLowerCase() === "asc" && ASCPokemons();
@@ -40,7 +166,7 @@ export const PokemonsLoad = () => {
 
   useEffect(() => {
     const getPokemons = async () => {
-      if (loading) return;
+      if (loading || !inView) return;
       setLoading(true);
 
       try {
@@ -54,146 +180,15 @@ export const PokemonsLoad = () => {
       }
     };
 
-    if (inView) {
-      getPokemons();
-    }
-  }, [inView, pagination, loading, pokemons]);
+    getPokemons();
+  }, [inView, pagination, loading]);
 
   return (
     <>
       <section className={styles.pokemons}>
-        {pokemons
-          .filter((pokemon) => {
-            if (!search?.trim() || search == null) {
-              return true;
-            } else if (
-              pokemon.name.toLowerCase().includes(search.toLowerCase())
-            ) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const fromVerification =
-              from != null ? pokemon.id >= from : pokemon.id >= 0;
-            const toVerification =
-              to != null ? pokemon.id <= to : pokemon.id <= MAXPOKEMONSRENDERED;
-
-            if (fromVerification && toVerification) {
-              return true;
-            }
-
-            if (from == null && to == null) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const types = pokemon.types.map((type) => type.type.name);
-            const currentType = type?.split(",") as PokemonPossibleTypes[];
-            const typeExists = currentType?.every((type) =>
-              types?.includes(type)
-            );
-
-            if (currentType?.length > 0 && typeExists) {
-              return true;
-            }
-
-            if (currentType == null) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const weaknesses = pokemon.weakness;
-            const currentWeakness = weakness?.split(
-              ","
-            ) as PokemonPossibleTypes[];
-            const weaknessExists = currentWeakness?.every((type) =>
-              weaknesses?.includes(type)
-            );
-
-            if (currentWeakness?.length > 0 && weaknessExists) {
-              return true;
-            }
-
-            if (currentWeakness == null) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const abilityList = pokemon.abilities.map((ability) =>
-              ability.ability.name.toLowerCase()
-            );
-            const currentAbility = ability as string;
-
-            if (
-              ability != null &&
-              abilityList.includes(currentAbility.toLowerCase())
-            ) {
-              return true;
-            }
-
-            if (ability == null || ability == undefined) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const currentWeight = weight;
-            const pokemonHeight = pokemon.weight / 10;
-
-            const minVerification =
-              currentWeight != null &&
-              pokemonHeight >
-                SELECTPOKEMONWEIGHTS[currentWeight - 1]?.range?.min;
-            const maxVerification =
-              currentWeight != null &&
-              pokemonHeight <=
-                SELECTPOKEMONWEIGHTS[currentWeight - 1]?.range?.max;
-
-            if (minVerification && maxVerification) {
-              return true;
-            }
-
-            if (currentWeight == null) {
-              return true;
-            }
-
-            return false;
-          })
-          .filter((pokemon) => {
-            const currentHeight = height;
-            const pokemonHeight = pokemon.height / 10;
-
-            const minVerification =
-              currentHeight != null &&
-              pokemonHeight >
-                SELECTPOKEMONHEIGHTS[currentHeight - 1]?.range?.min;
-            const maxVerification =
-              currentHeight != null &&
-              pokemonHeight <=
-                SELECTPOKEMONHEIGHTS[currentHeight - 1]?.range?.max;
-
-            if (minVerification && maxVerification) {
-              return true;
-            }
-
-            if (currentHeight == null) {
-              return true;
-            }
-
-            return false;
-          })
-          .map((pokemon, index) => {
-            return <PokemonCard key={index} {...pokemon} />;
-          })}
+        {filteredPokemons.map((pokemon, index) => {
+          return <PokemonCard key={index} {...pokemon} />;
+        })}
       </section>
       {MAXPOKEMONSRENDERED + POKEMONSPERPAGE > POKEMONSPERPAGE * pagination && (
         <div ref={loadingRef} className={styles.loader}>
